46c98922aae6bda61e1cf1f806e92985ed3c7b02
GODARA7 <subhashgodara237057@gmail.com>
2024-12-09 15:26:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def send_random_photo(bot, chat_id):
      height = random.randint(600, 800)
      await bot.send_photo(
          chat_id=chat_id,
-         photo=f"https://picsum.photos/{width}/{height}.jpg",
+         photo=f"https://images.app.goo.gl/ztF6hXni7Qw5BhnU8",
          caption=f"𝐇𝐞𝐥𝐥𝐨 𝐃𝐞𝐚𝐫 👋!\n\n➠ 🌟 𝐈 AM UPLOADER BOT 🕹️ \n\n➠ 📛 Can Extract Videos & Pdf Form Your Text File and Upload to Telegram \n\n➠ 🍁 𝐔𝐬𝐞 /tarain 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐓𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐅𝐫𝐨𝐦 𝐓𝐗𝐓 𝐅𝐢𝐥𝐞 \n\nFor Stop ⛔ working process ➡️ /stop  \n\n<pre>➠ 💗 𝐌𝐚𝐝𝐞 𝐁𝐲: 🔰 『𝐓𝐇𝐄𝐑𝐌𝐎𝐏𝐎𝐋𝐘™ 🥷🐦‍🔥』 🌎</pre>\n-═════━‧₊˚❀༉‧₊˚.━═════-\n",
          reply_markup=keyboard
      )
